6f9d64822507865882403f1093297f3caec0aa2b
sol <sol.recieve@gmail.com>
2023-06-06 19:09:30 +0200

    day3b

--- day3/src/b.rs
+++ day3/src/b.rs
@@ -1,3 +1,23 @@
-fn main() {
+fn priority(c: &u8) -> i16 {
+    if *c > b'a' {
+        (*c - b'a') as i16 + 1
+    } else {
+        (*c - b'A') as i16 + 27
+    }
+}
 
+fn main() {
+    let solution = include_bytes!("../res/input.txt")
+        .split(|c| *c == b'\n')
+        .collect::<Vec<_>>()
+        .chunks(3)
+        .map(|rucksacks|
+            rucksacks[0]
+                .iter()
+                .find(|c| rucksacks[1].contains(c) && rucksacks[2].contains(c))
+                .unwrap())
+        .map(|c| priority(c))
+        .sum::<i16>();
+
+    println!("{}", solution)
 }
